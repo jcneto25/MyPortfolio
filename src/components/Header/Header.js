1fec353ce49df4bbb34eb4149fb8153e47fd1eda
Jaime  Correia Neto <jcneto25@gmail.com>
2022-03-14 12:30:39 -0300

    Correção de abertura de link via popup

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,47 +1,64 @@
-import Link from 'next/link';
-import React from 'react';
-import { AiFillGithub, AiFillInstagram, AiFillLinkedin, AiFillMail } from 'react-icons/ai';
-import { DiCssdeck } from 'react-icons/di';
+import Link from "next/link";
+import React from "react";
+import {
+  AiFillGithub,
+  AiFillInstagram,
+  AiFillLinkedin,
+  AiFillMail,
+} from "react-icons/ai";
+import { DiCssdeck } from "react-icons/di";
 
-import { Container, Div1, Div2, Div3, NavLink, SocialIcons, Span } from './HeaderStyles';
+import {
+  Container,
+  Div1,
+  Div2,
+  Div3,
+  NavLink,
+  SocialIcons,
+  Span,
+} from "./HeaderStyles";
 
-const Header = () =>  (
+const Header = () => (
   <Container>
     <Div1>
-        <Link href="/">
-          <a style={{display: "flex", alignItems: "center", color: "white", marginBottom: "15px"}}>
-            <DiCssdeck size="3rem" /> <Span>  Portfolio </Span>
-          </a>
-        </Link>
+      <Link href="/">
+        <a
+          style={{
+            display: "flex",
+            alignItems: "center",
+            color: "white",
+            marginBottom: "15px",
+          }}
+        >
+          <DiCssdeck size="3rem" /> <Span> Portfolio </Span>
+        </a>
+      </Link>
     </Div1>
     <Div2>
       <li>
         <Link href="#projetos">
-          <NavLink>
-            Projetos
-          </NavLink>
+          <NavLink>Projetos</NavLink>
         </Link>
       </li>
       <li>
         <Link href="#tech">
-          <NavLink>
-            Tecnologias
-          </NavLink>
+          <NavLink>Tecnologias</NavLink>
         </Link>
       </li>
       <li>
         <Link href="#about">
-          <NavLink>
-            Sobre
-          </NavLink>
+          <NavLink>Sobre</NavLink>
         </Link>
       </li>
     </Div2>
     <Div3>
-      <SocialIcons href="http://github.com/jcneto25">
+      <SocialIcons href="http://github.com/jcneto25" target="_blank">
         <AiFillGithub size="3rem" />
       </SocialIcons>
-      <SocialIcons href="https://www.linkedin.com/in/jaime-correia-neto">
+      <SocialIcons
+        href="https://www.linkedin.com/in/jaime-correia-neto"
+        target="_blank"
+      >
         <AiFillLinkedin size="3rem" />
       </SocialIcons>
       <SocialIcons href="mailto:jcneto25@gmail.com">
