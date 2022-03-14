1897e26887954a9a4d3f900e468f1ce9badf50af
Jaime  Correia Neto <jcneto25@gmail.com>
2022-03-14 11:11:40 -0300

    Correção do Link do botão do CV

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,25 +1,31 @@
-import React from 'react';
+import React from "react";
 
-import { Section, SectionText, SectionTitle } from '../../styles/GlobalComponents';
-import Button from '../../styles/GlobalComponents/Button';
-import { LeftSection } from './HeroStyles';
+import {
+  Section,
+  SectionText,
+  SectionTitle,
+} from "../../styles/GlobalComponents";
+import Button from "../../styles/GlobalComponents/Button";
+import { LeftSection } from "./HeroStyles";
 
 const Hero = (props) => (
-<Section row nopadding> 
-  <LeftSection>
-    <SectionTitle main center>
-      Bem-Vindo ao <br/>
-      meu portfolio pessoal.
-    </SectionTitle>
-    <SectionText>
-      Projetos em Cloud, DataScience, Python e React para alavancar o seu negócio.
-    </SectionText>
-    <Button onClick={() => window.location = 'https://google.com'}>
-      Conheça Mais
-    </Button>
-  </LeftSection>
-</Section>
-
+  <Section row nopadding>
+    <LeftSection>
+      <SectionTitle main center>
+        Bem-Vindo ao <br />
+        meu portfolio pessoal.
+      </SectionTitle>
+      <SectionText>
+        Projetos em Cloud, DataScience, Python e React para alavancar o seu
+        negócio.
+      </SectionText>
+      <Button
+        onClick={() => window.open("https://jcneto25.github.io/MyResume/")}
+      >
+        Conheça meu CV / See my Resume
+      </Button>
+    </LeftSection>
+  </Section>
 );
 
 export default Hero;
