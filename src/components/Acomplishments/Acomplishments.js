226404e65e2622ad4b4360aacdceed521d18bd25
Jaime  Correia Neto <jcneto25@gmail.com>
2022-03-14 15:43:43 -0300

    Adicionado referência às certificações Oracle

--- src/components/Acomplishments/Acomplishments.js
+++ src/components/Acomplishments/Acomplishments.js
@@ -1,16 +1,24 @@
-import React from 'react';
+import React from "react";
 
-import { Section, SectionDivider, SectionTitle } from '../../styles/GlobalComponents';
-import { Box, Boxes, BoxNum, BoxText } from './AcomplishmentsStyles';
+import {
+  Section,
+  SectionDivider,
+  SectionTitle,
+} from "../../styles/GlobalComponents";
+import { Box, Boxes, BoxNum, BoxText } from "./AcomplishmentsStyles";
 
 const data = [
-  { number: 1998, text: 'Certificação Conectiva Linux', },
-  { number: 2000, text: 'Graduado em Informática pela UNIFOR'},
-  { number: 2001, text: 'Especialização em Gerência Estratégica da Informação', },
-  { number: 2003, text: 'MBA Excutivo IBMEC', },
-  { number: 2007, text: 'Treinamentos e participação em projetos em Java', },
-  { number: 2018, text: 'Graduação em Direito', },
-  { number: 2019, text: 'Formação Python na Alura', }
+  { number: 1998, text: "Certificação Conectiva Linux" },
+  { number: 2000, text: "Graduado em Informática pela UNIFOR" },
+  {
+    number: 2001,
+    text: "Especialização em Gerência Estratégica da Informação",
+  },
+  { number: 2003, text: "MBA Excutivo IBMEC" },
+  { number: 2007, text: "Treinamentos e participação em projetos em Java" },
+  { number: 2018, text: "Graduação em Direito" },
+  { number: 2019, text: "Formação Python na Alura" },
+  { number: 2022, text: "Certificações Cloud Oracle" },
 ];
 
 const Acomplishments = () => (
@@ -21,7 +29,6 @@ const Acomplishments = () => (
         <Box key={index}>
           <BoxNum>Ano {card.number}</BoxNum>
           <BoxText>{card.text}</BoxText>
-
         </Box>
       ))}
     </Boxes>
