a7d8450190fc707052873cc0fc9c5d7d32393179
Jaime Correia <jcneto25@gmail.com>
2022-11-18 09:49:19 -0300

    Adição do projeto bag-store no portfolio

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -9,12 +9,12 @@ export const projects = [
     id: 0,
   },
   {
-    title: 'E-Commerce',
-    description:"While building it you're going to learn many advanced React & JavaScript topics, as well as how to use Stripe for card transactions. On top of that, at the end of the video, you will have this unique and complex webshop app that you will be able to add to your portfolio. And trust me, e-commerce applications are impressive.",
+    title: 'E-Commerce Bag Store',
+    description:"E-Commerce site builted with React and Material UI. It also uses react hooks such as useState and useContext. It's a responsive site and can be used on different screen sizes. I used Eslint e Prettier on the dev environment setup for this project.",
     image: '/images/2.png',
-    tags: ['React', 'JavaScript'],
-    source: 'https://google.com',
-    visit: 'https://google.com',
+    tags: ['React', 'JavaScript', 'Material UI', 'Vite'],
+    source: 'https://bag-store.vercel.app',
+    visit: 'https://bag-store.vercel.app',
     id: 1,
   },
   {
