a4fc214d10134618a9d472b0722b16061326093a
Jaime Correia <jcneto25@gmail.com>
2023-01-24 19:20:53 -0300

    Icone ZAP

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { AiFillGithub, AiFillInstagram, AiFillLinkedin } from 'react-icons/ai';
+import { AiFillGithub, AiFillInstagram, AiFillLinkedin, AiOutlineWhatsApp } from 'react-icons/ai';
 
 import { SocialIcons } from '../Header/HeaderStyles';
 import { CompanyContainer, FooterWrapper, LinkColumn, LinkItem, LinkList, LinkTitle, Slogan, SocialContainer, SocialIconsContainer } from './FooterStyles';
@@ -9,9 +9,10 @@ const Footer = () => {
     <FooterWrapper>
       <LinkList>
         <LinkColumn>
-          <LinkTitle>Contato</LinkTitle>
+          <LinkTitle>Whatsapp</LinkTitle>
           <LinkItem href="https://wa.me/+5585989519450?text=Olá%20!%20Visitei%20seu%20Portfolio.%20Gostaria%20de%20obter%20mais%20informações."
             target="_blank">+55 85 98951-9450</LinkItem>
+            <AiOutlineWhatsApp size={50} color="rgb(50, 120, 148)" className="icon" />
         </LinkColumn>
         <LinkColumn>
           <LinkTitle>E-mail</LinkTitle>
