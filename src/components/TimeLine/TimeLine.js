ee35edcdb450cf39db22351cd03a16c9a44a46d0
Jaime Correia <jcneto25@gmail.com>
2025-04-15 08:50:13 -0300

    corrigir erro de português

--- src/components/TimeLine/TimeLine.js
+++ src/components/TimeLine/TimeLine.js
@@ -10,41 +10,41 @@ const BASEROW_API_TOKEN = process.env.NEXT_PUBLIC_BASEROW_API_TOKEN;
 console.log("BASEROW_API_TOKEN:", process.env.NEXT_PUBLIC_BASEROW_API_TOKEN);
 
 const Timeline = ({ timelineData }) => {
-   const [activeItem, setActiveItem] = useState(0);
-   const carouselRef = useRef();
+  const [activeItem, setActiveItem] = useState(0);
+  const carouselRef = useRef();
   const TOTAL_CAROUSEL_COUNT = timelineData.length;
 
-   const scroll = (node, left) => {
-     return node.scrollTo({ left, behavior: 'smooth' });
-   }
+  const scroll = (node, left) => {
+    return node.scrollTo({ left, behavior: 'smooth' });
+  }
 
-   const handleClick = (e, i) => {
-     e.preventDefault();
+  const handleClick = (e, i) => {
+    e.preventDefault();
 
-     if (carouselRef.current) {
-       const scrollLeft = Math.floor(carouselRef.current.scrollWidth * 0.7 * (i / timelineData.length)); // Use timelineData instead of TimeLineData
-      
-       scroll(carouselRef.current, scrollLeft);
-     }
-   }
+    if (carouselRef.current) {
+      const scrollLeft = Math.floor(carouselRef.current.scrollWidth * 0.7 * (i / timelineData.length)); // Use timelineData instead of TimeLineData
 
-   const handleScroll = () => {
-     if (carouselRef.current) {
-       const index = Math.round((carouselRef.current.scrollLeft / (carouselRef.current.scrollWidth * 0.7)) * timelineData.length); // Use timelineData instead of TimeLineData
+      scroll(carouselRef.current, scrollLeft);
+    }
+  }
 
-       setActiveItem(index);
-     }
-   }
+  const handleScroll = () => {
+    if (carouselRef.current) {
+      const index = Math.round((carouselRef.current.scrollLeft / (carouselRef.current.scrollWidth * 0.7)) * timelineData.length); // Use timelineData instead of TimeLineData
+
+      setActiveItem(index);
+    }
+  }
 
   // // snap back to beginning of scroll when window is resized
   // // avoids a bug where content is covered up if coming from smaller screen
-   useEffect(() => {
-     const handleResize = () => {
-       scroll(carouselRef.current, 0);
-     }
+  useEffect(() => {
+    const handleResize = () => {
+      scroll(carouselRef.current, 0);
+    }
 
-     window.addEventListener('resize', handleResize);
-   }, []);
+    window.addEventListener('resize', handleResize);
+  }, []);
 
   return (
     <Section id="about">
@@ -52,22 +52,22 @@ const Timeline = ({ timelineData }) => {
       <SectionText>
         Profissional de Tecnologia atuando no mercado desde 1996. Atualmente gerencio a unidade de desenvolvimento dinâmico do Tribunal de Justiça do Ceará, onde aplicamos
         tecnologias de low-code/no-code aliadas a ferramentas de automação de fluxos de negócios.
-        Conhecimento no desenvolvimento de aplicações Web/Mobile com React/Next.js e Tailwind. Utilização de backend com CMS Headless ou desenvolvido com 
+        Conhecimento no desenvolvimento de aplicações Web/Mobile com React/Next.js e Tailwind. Utilização de backend com CMS Headless ou desenvolvido com
         Node.js/Express. Aplicação de técnicas de SEO para garantir que a marca do cliente esteja sempre em destaque nos resultados de pesquisa.
-        Conehcimento em plataformas Cloud e como elas podem auxiliar as empresas a reduzir custos na operação de seus sistemas, bem como usar serviços
+        Conhecimento em plataformas Cloud e como elas podem auxiliar as empresas a reduzir custos na operação de seus sistemas, bem como usar serviços
         de exploração de dados.
       </SectionText>
       <CarouselContainer ref={carouselRef} onScroll={handleScroll}>
-      <>
-        {timelineData.map((item, index) => (
-          <CarouselMobileScrollNode key={index} final={index===TOTAL_CAROUSEL_COUNT -1}>
-            <CarouselItem
-              index={index}
-              id={`carousel__item-${index}`}
-              active={activeItem}
-              onClick={(e)=> handleClick(e, index)}
-            >
-              <CarouselItemTitle>
+        <>
+          {timelineData.map((item, index) => (
+            <CarouselMobileScrollNode key={index} final={index === TOTAL_CAROUSEL_COUNT - 1}>
+              <CarouselItem
+                index={index}
+                id={`carousel__item-${index}`}
+                active={activeItem}
+                onClick={(e) => handleClick(e, index)}
+              >
+                <CarouselItemTitle>
                   {item.Ano} {/* Assuming 'year' is the field name in your Baserow table */}
                   <CarouselItemImg
                     width="208"
@@ -99,25 +99,25 @@ const Timeline = ({ timelineData }) => {
                       </linearGradient>
                     </defs>
                   </CarouselItemImg>
-              </CarouselItemTitle>
-              <CarouselItemText>{item.conquista} {/* Assuming 'text' is the field name in your Baserow table */}</CarouselItemText>
-            </CarouselItem>
-          </CarouselMobileScrollNode> 
-        ))}
-      </>
+                </CarouselItemTitle>
+                <CarouselItemText>{item.conquista} {/* Assuming 'text' is the field name in your Baserow table */}</CarouselItemText>
+              </CarouselItem>
+            </CarouselMobileScrollNode>
+          ))}
+        </>
       </CarouselContainer>
       <CarouselButtons>
-      {timelineData.map((item, index) => (
-        <CarouselButton
-          key={index}
-          index={index}
-          active={activeItem}
-          onClick={(e)=> handleClick(e, index)}
-          type="button"
-        >
-          <CarouselButtonDot active={activeItem} />
-        </CarouselButton>
-      ))}
+        {timelineData.map((item, index) => (
+          <CarouselButton
+            key={index}
+            index={index}
+            active={activeItem}
+            onClick={(e) => handleClick(e, index)}
+            type="button"
+          >
+            <CarouselButtonDot active={activeItem} />
+          </CarouselButton>
+        ))}
       </CarouselButtons>
       <SectionDivider />
     </Section>
